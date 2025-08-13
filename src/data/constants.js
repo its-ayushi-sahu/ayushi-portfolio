332414c0a8e63b2645646e03f28f66b9246b58b7
Ayushi Sahu <ayushi123sa@gmail.com>
2025-08-14 00:22:50 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,8 +1,8 @@
 export const Bio = {
   name: "Ayushi Sahu",
   roles: ["Full Stack Developer", "Data Analyst", "Data Scientist"],
-  description1:
-    "I’m a passionate and detail-oriented Data Analyst & Dashboard Developer who enjoys transforming complex data into clear, actionable insights. With hands-on experience in data visualization, business intelligence, and basic machine learning, I’ve worked across domains like finance, HR, sales forecasting, fraud detection, and social media analysis — helping teams make smarter, data-driven decisions.",
+  description1: "I'm a versatile professional skilled in full-stack development, data analysis, and data science. I combine technical expertise in front-end and back-end development with a strong analytical mindset to build scalable applications, uncover data-driven insights, and develop predictive models that drive business impact.",
+   // "I’m a passionate and detail-oriented Data Analyst & Dashboard Developer who enjoys transforming complex data into clear, actionable insights. With hands-on experience in data visualization, business intelligence, and basic machine learning, I’ve worked across domains like finance, HR, sales forecasting, fraud detection, and social media analysis — helping teams make smarter, data-driven decisions.",
   description2:
     "Beyond the tools and tech, I’m a curious problem-solver who loves exploring patterns, asking the right questions, and presenting insights through compelling, easy-to-understand dashboards. I take pride in combining analytical thinking with visual storytelling to bring clarity to complex challenges.",
   description3:

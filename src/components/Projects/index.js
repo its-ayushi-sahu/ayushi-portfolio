91799f404d79f003b746cb33ecbf0b15f1c0968b
Ayushi Sahu <ayushi123sa@gmail.com>
2025-09-25 22:27:40 +0530

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -14,7 +14,7 @@ import {
 
 const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState("all");
-  const toggleOptions = ["MERN", "DATA ANALYTICS", "DATA SCIENCE"];
+  const toggleOptions = ["DATA SCIENCE", "DATA ANALYTICS", "MERN"];
 
   return (
     <Container id="projects">

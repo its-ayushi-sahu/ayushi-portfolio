2645bb0b9747c585e626e4151770e7eeb134ab66
Ayushi Sahu <ayushi123sa@gmail.com>
2025-09-25 22:32:57 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 export const Bio = {
   name: "Ayushi Sahu",
-  roles: ["Full Stack Developer", "Data Analyst", "Data Scientist"],
-  description1: "I'm a versatile professional skilled in full-stack development, data analysis, and data science. I combine technical expertise in front-end and back-end development with a strong analytical mindset to build scalable applications, uncover data-driven insights, and develop predictive models that drive business impact.",
+  roles: ["Full Stack Developer", "Data Scientist", "Data Analyst", "Data Scientist"],
+  description1: "I'm a versatile professional skilled in data science, data analysis, and full-stack development. I combine technical expertise in data science and analytics with a strong analytical mindset to build scalable applications, uncover data-driven insights, and develop predictive models that drive business impact.",
    // "I’m a passionate and detail-oriented Data Analyst & Dashboard Developer who enjoys transforming complex data into clear, actionable insights. With hands-on experience in data visualization, business intelligence, and basic machine learning, I’ve worked across domains like finance, HR, sales forecasting, fraud detection, and social media analysis — helping teams make smarter, data-driven decisions.",
   description2:
     "Beyond the tools and tech, I’m a curious problem-solver who loves exploring patterns, asking the right questions, and presenting insights through compelling, easy-to-understand dashboards. I take pride in combining analytical thinking with visual storytelling to bring clarity to complex challenges.",
@@ -9,7 +9,7 @@ export const Bio = {
     "I thrive in collaborative environments, continuously seek to learn, and believe that good data doesn’t just explain the past — it helps shape better decisions for the future.",
   github: "https://github.com/its-ayushi-sahu",
   resume:
-    "https://drive.google.com/file/d/177QUgcMG71WWgtrQwQqq1pI8fM5Ci61-/view?usp=sharing",
+    "https://drive.google.com/file/d/13GNwVMGAdDGND2UhPVujPqTM-tnr3fBR/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/its-ayushi-sahu/",
   twitter: "https://www.linkedin.com/in/its-ayushi-sahu/",
   insta: "https://www.linkedin.com/in/its-ayushi-sahu/",
